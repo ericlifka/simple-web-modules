e6035e42484f1c7514ab271b953337d4c591fb7c
Eric Lifka <eric.lifka@gmail.com>
2016-04-27 13:04:16 -0400

    test that main is run

--- test/modules.js
+++ test/modules.js
@@ -2,8 +2,23 @@ var should = require('should');
 var SM = require('../index');
 
 describe('SM Modules', function () {
+
   it('should exist', function () {
     should.exist(SM);
     SM.should.have.property('DefineModule');
   });
+
+  it('should run a provided main module', function (done) {
+    var called = false;
+    SM.DefineModule('main', function () {
+      called = true;
+    });
+
+    called.should.equal(false);
+    SM.runMain();
+    called.should.equal(true);
+
+    done();
+  });
+
 });
