d7e3cb5d54b5bd5f2f7083eabe50d5cbc4b8eaed
Eric Lifka <eric.lifka@gmail.com>
2016-04-28 12:46:02 -0400

    throw in some easy tests for event marking

--- test/classes.js
+++ test/classes.js
@@ -1,7 +1,21 @@
 var should = require('should');
+var SM = require('../index');
 
-describe('test', function () {
-  it('thing', function () {
-    (5).should.be.exactly(5).and.be.a.Number;
+describe('SM Classes', function () {
+
+  describe('SM.event', function () {
+    it('should return the same function', function () {
+      var fn = function () {};
+      var ret = SM.event(fn);
+
+      ret.should.be.exactly(fn);
+    });
+
+    it('should set an identifier on the function', function () {
+      var fn = function () {};
+      SM.event(fn);
+
+      fn.should.have.property('isSMEvent');
+    });
   });
 });
